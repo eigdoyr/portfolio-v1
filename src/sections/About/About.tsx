9f5520331aa16eb3ab6568e1ffdda447cbd833fb
Ryodgie Barnatia <ryodgie@gmail.com>
2026-04-05 22:08:30 +0800

    feat: add About page placeholder

---
+++ src/sections/About/About.tsx
@@ -0,0 +1,14 @@
+import PageTransition from "../../components/PageTransition/PageTransition";
+import "./About.css";
+
+const About = () => (
+  <PageTransition>
+    <main className="about-main">
+      <div className="about-placeholder">
+        <span>Coming Soon</span>
+      </div>
+    </main>
+  </PageTransition>
+);
+
+export default About;
