4d5889994644a3d99ffbcd059cee7bfbb07a0ee1
Ryodgie Barnatia <ryodgie@gmail.com>
2026-04-11 21:58:27 +0800

    chore: add vite-plugin-image-optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     react(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      jpg: { quality: 80 },
+      webp: { quality: 80 },
+    }),
     compression({ algorithm: "brotliCompress", ext: ".br" }),
     compression({ algorithm: "gzip", ext: ".gz" }),
   ],
